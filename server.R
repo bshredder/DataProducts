30aae4702763cd4b1d315737eed2e73745b0022e
unknown <billschroederinc@gmail.com>
2015-12-27 08:43:08 -0600

    changed plotting framework to use ggplot

--- server.R
+++ server.R
@@ -11,50 +11,57 @@ library(base64enc)
 # share across all sessions
 source('twitter_sentiment_analysis.R', local=TRUE)
 
-options(httr_oauth_cache=T)
 authenticate_twitterAPI()
+sentiment_list_pos = scan('positive-words.txt', what='character', comment.char=';')
+sentiment_list_neg = scan('negative-words.txt', what='character', comment.char=';')
 
 shinyServer(
   
   function(input, output, session){
-    
-    obs <- observe({    
-      
-      # retrieve the tweets associated with the user's input 
-      get_tweets( as.character(input$user_input_value), 200 )
-      
-      # calculate the sentiment of the tweets retrieved
-      #reactive( { calculate_sentiment() } )  
-      calculate_sentiment()          
-      
-      # store and send back to ui the sentiment table generated
-      output$tableOutputID <- renderDataTable( { as.data.frame(twEnv$sentiment_scores) } )      
-      
-      #output$prediction <- renderPrint({paste(input$user_telemetry_value, twEnv$sentiment_scores$score) })
-      output$sentimentHist <- renderPlot({
-        hist( twEnv$sentiment_scores$score, 
-              xlab='Sentiment Score', 
-              col='lightblue', 
-              main = paste("Sentiment Analysis of ", input$user_input_value) )
-        m <- mean( twEnv$sentiment_scores$score )
-        lines(c(m, m), c(0, 200),col="red",lwd=5)
-        text(40, 75, paste("mean = ", m))
+
+    obs <- observe({      
+
+      # cat( "Enter observe()", input$user_input_value )
+
+      if(input$user_input_value != ""){
         
-      })      
+        # retrieve the tweets associated with the user's input 
+        shiny_tweetList <- get_tweets( input$user_input_value, 20 )
+        
+        cat( "shiny_tweetList", length(shiny_tweetList$text) )            
+        if( length(shiny_tweetList$text) > 0){
+          
+          # calculate the sentiment of the tweets retrieved
+          shiny_sentiment_scores <- calculate_sentiment(shiny_tweetList$text, sentiment_list_pos, sentiment_list_neg)          
+          
+          output$sentimentHist <- renderPlot({
+      
+            validate(
+              need(shiny_sentiment_scores$score, 'Sorry ;-( no sentiment scores could be calculated')
+            )      
+            
+            ggplot(data = shiny_sentiment_scores, aes( score ) ) +
+              geom_histogram(breaks=seq(-6,6,by= 1), col="red", fill="green", alpha = .2) + 
+              geom_vline( xintercept = mean( shiny_sentiment_scores$score), size = 1.5, color="black", linetype = "longdash") +
+              labs(title=paste("Sentiment Analysis Histogram of ", input$user_input_value )) +
+              labs(x="sentiment Score", y="Count") 
+          })
+          
+          # store and send back to ui the sentiment table generated
+          output$tableOutputID <- renderDataTable( { as.data.frame( shiny_sentiment_scores ) } )   
+        }
+      }
+      else{
+        rm(shiny_tweetList)
+        rm(shiny_sentiment_scores)
+      }
+      
+      print( "Exit observe()" )
     })
     
-    # When the client ends the session, suspend the observer
-    session$onSessionEnded(function() {
-      obs$suspend()
-    })     
-  
-   
     # store and send back to ui the user's original input value 
     output$inputValue <- renderPrint({
       input$user_input_value
-    })     
-    
-
-    
-    
+    })      
+ 
   })
